0e633fa8390961dd92e9cb20af68096664c5e2c2
Nicholas Zivkovic <znichola@student.42lausanne.ch>
2023-06-24 18:23:59 +0200

    going home

--- NPC/read.py
+++ NPC/read.py
@@ -4,19 +4,16 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from dataclasses import dataclass
 
+# gen list of words
+def get_word_list():
+	with open('given/USACONST.TXT', encoding='ISO8859') as f:
+		text = f.read()
+	return set(re.sub('[^a-z]', ' ', text.lower()).split())
+
 # Create an empty graph
 G = nx.Graph()
 
-@dataclass
-class Node:
-    letter: str
-    id: int
-
-@dataclass
-class Edge:
-    a: int
-    b: int
-
+hasher = get_word_list()
 
 def foo(path):
 	# Open the file
@@ -29,78 +26,75 @@ def foo(path):
 	# Extract edges using regular expressions
 	edges = re.findall(r'(\d+)\s*--\s*(\d+);', content)
 
-	# Create a dictionary to store nodes
-	nodes = {}
-
 	# Create Node objects and store them in the dictionary
 	for node_id, label in node_labels:
-		node = Node(label, int(node_id))
-		nodes[node_id] = label
-
-	# Create a list to store edges
-	graph_edges = []
+		G.add_node(int(node_id), label=label)
 
 	# Create Edge objects and append them to the list
 	for node_a_id, node_b_id in edges:
-		# node_a = nodes[int(node_a_id)]
-		# node_b = nodes[int(node_b_id)]
-		edge = Edge(node_a_id, node_b_id)
-		graph_edges.append(edge)
+		G.add_edge(int(node_a_id), int(node_b_id))
 
-	# Print the nodes
-	print("Nodes:")
-	for node_id in nodes:
-		id : int = node_id
-		lb : str = nodes[node_id]
-		G.add_node(id, label=lb)
-		print(f"ID: {id}, Letter: {lb}")
+
+# Print the nodes
+def print_graph(g):
+	print("Node:")
+	for node in G.nodes:
+		label = G.nodes[node]['label']  # Get the label of the node
+		print(f"ID: {node}, Label: {label}")
 
 	# Print the edges
 	print("Edges:")
-	for edge in graph_edges:
-		G.add_edge(edge.a, edge.b)
-		print(f"A: {edge.a}, B: {edge.b}")
+	for edge in g.edges:
+		print(edge)
 
 
 # Draw the graph
-def draw_graph(graph):
-	pos = nx.spring_layout(graph)  # Position the nodes using a spring layout algorithm
-	labels = nx.get_node_attributes(graph, 'label')  # Get the node labels
+def draw_graph(g):
+	pos = nx.spring_layout(g)  # Position the nodes using a spring layout algorithm
+	labels = nx.get_node_attributes(g, 'label')  # Get the node labels
 	# print(nx.adjacency_matrix(G))
-	nx.draw_networkx(graph, pos, labels=labels)
+	nx.draw_networkx(g, pos, labels=labels)
 	plt.show()
 
 
-# Depth-First Search (DFS)
-def dfs(graph, start_node):
+
+
+def dfs(node): # a list of node paths starting form the node
+
+	# Perform DFS traversal
 	visited = set()  # Set to keep track of visited nodes
-	stack = [start_node]  # Stack for DFS traversal
 
-	while stack:
-		node = stack.pop()
-		if node not in visited:
-			print(node)
-			visited.add(node)
-			neighbors = graph.neighbors(node)  # Get neighbors of the current node
-			stack.extend(neighbors)
+	visited.add(node)
+
+	neighbors = G.neighbors(node)  # Get neighbors of the current node
+	for neighbor in neighbors:
+		if neighbor not in visited:
+			print("visited: ", neighbor)
+			dfs(neighbor)
 
 
-# # Perform DFS traversal
-# visited = set()  # Set to keep track of visited nodes
+# def generate_and_test(vertices, subset):
+# 	print("testing:", vertices)
+# 	if not vertices:
+# 		# Check if the subset induces a connected subgraph
+# 		if nx.is_connected(G.subgraph(subset)):
+# 			yield subset
+# 	else:
+# 		v = vertices[0]  # Choose a vertex v from the remaining vertices
+# 		generate_and_test(vertices[1:], subset)
+# 		generate_and_test(vertices[1:], subset + [v])
 
-# def dfs(graph, node):
-#     visited.add(node)
-#     print(node)
-
-#     neighbors = graph.neighbors(node)  # Get neighbors of the current node
-#     for neighbor in neighbors:
-#         if neighbor not in visited:
-#             dfs(graph, neighbor)
-
-# # Call the DFS function starting from node 1
-# dfs(G, 1)
 
 if __name__ == '__main__':
-  foo(path=sys.argv[1].encode('utf-8'))
-  dfs(G, 1)
-  draw_graph(G)
+	foo(path=sys.argv[1].encode('utf-8'))
+	print_graph(G)
+
+	all_nodes = list(G.nodes)
+
+	dfs(all_nodes[0])
+	# subsets = generate_and_test(all_nodes, [])
+	# print("Connected Subsets:")
+	# for subset in subsets:
+	# 	print(subset)
+
+#   draw_graph(G)
