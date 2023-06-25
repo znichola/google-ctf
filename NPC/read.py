2d9b50311cb55b56e66ed9993413f8c4a994b3d0
znichola <niki.zivkovic@hotmail.com>
2023-06-25 14:16:08 +0200

    Decoded a single word password!
    - visited all possibilities of the graph and check against the dic of
      words, and it works!

--- NPC/read.py
+++ NPC/read.py
@@ -54,7 +54,8 @@ def draw_graph(g):
 	labels = nx.get_node_attributes(g, 'label')  # Get the node labels
 	# print(nx.adjacency_matrix(G))
 	nx.draw_networkx(g, pos, labels=labels)
-	plt.show()
+	# plt.show()
+	plt.savefig("mygraph.png")
 
 
 
@@ -85,16 +86,54 @@ def dfs(node): # a list of node paths starting form the node
 # 		generate_and_test(vertices[1:], subset + [v])
 
 
+def visit(neighbors : set, to_visit : set, visited : list):
+	# print(to_visit)
+	# print(neibours)
+	for n in neighbors:
+		if n in to_visit:
+			to_visit.remove(n)
+			visited.append(n)
+			if (len(visited) == len(G.nodes)):
+				word = ''.join(str(G.nodes[i]['label']) for i in visited)
+				if word in hasher:
+					print(word)
+				return
+			# print(G.nodes[n]['label'], end='')
+			visit(set(G.neighbors(n)), set(to_visit), list(visited))
+			visited.pop()
+			to_visit.add(n)
+			# if len(to_visit) == 0:
+				# print("")
+
+
 if __name__ == '__main__':
 	foo(path=sys.argv[1].encode('utf-8'))
 	print_graph(G)
 
 	all_nodes = list(G.nodes)
 
-	dfs(all_nodes[0])
+	# dfs(all_nodes[0])
 	# subsets = generate_and_test(all_nodes, [])
 	# print("Connected Subsets:")
 	# for subset in subsets:
 	# 	print(subset)
 
-#   draw_graph(G)
+	# itterator for all the nodes
+	for n in G:
+		print(G.nodes[n]['label'], ": ", end='')
+
+		# for each node we find it's neighbours
+		for nei in G.neighbors(n):
+			print(G.nodes[nei]['label'], end='')
+		print("")
+
+	for n in G:
+		neighbors = set(G.neighbors(n))
+		to_visit = set(G.nodes)
+		to_visit.remove(n)
+		visited : list = [n]
+
+		# print("visiting : ", G.nodes[n]['label'], end=' ')
+		visit(neighbors, to_visit, visited)
+		# print("")
+	# draw_graph(G)
